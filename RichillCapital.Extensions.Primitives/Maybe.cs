74aacd7815b64516edb50d6374c82d0cf0795fb2
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-03 00:43:52 +0800

    Basic maybe with test

---
+++ RichillCapital.Extensions.Primitives/Maybe.cs
@@ -0,0 +1,22 @@
+namespace RichillCapital.Extensions.Primitives;
+
+public sealed class Maybe
+{
+    public static Maybe<TValue> From<TValue>(TValue value) => new(value);
+}
+
+public sealed class Maybe<TValue>
+{
+    internal Maybe(TValue value)
+    {
+        Value = value;
+    }
+
+    public TValue Value { get; private init; }
+
+    public bool HasValue => Value is not null;
+
+    public static implicit operator Maybe<TValue>(TValue value) => new(value);
+
+    public static implicit operator TValue(Maybe<TValue> maybe) => maybe.Value;
+}
